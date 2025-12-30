5c3796f15100fff4bd183fc64b6f69d62968ca73
Jack W <jackweatherilt@outlook.com>
2025-12-30 10:19:33 -0500

    Initial Commit

---
+++ JackApp/JackAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JackAppApp.swift
+//  JackApp
+//
+//  Created by Jack on 30/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct JackAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
